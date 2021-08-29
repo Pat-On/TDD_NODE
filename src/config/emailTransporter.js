aa3dbb5b786d3266c9e5a1d84b477a21236147d0
Patryk <patryk.r.nowak@gmail.com>
2021-08-29 16:28:50 +0100

    testing on web client

--- src/config/emailTransporter.js
+++ src/config/emailTransporter.js
@@ -1,14 +1,11 @@
 const nodemailer = require('nodemailer');
+const config = require('config');
+
+const mailConfig = config.get('mail');
 // const nodemailerStub = require('nodemailer-stub');
 
 // const transporter = nodemailer.createTransport(nodemailerStub.stubTransport);
 
-const transporter = nodemailer.createTransport({
-  host: 'localhost',
-  port: 8587,
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
+const transporter = nodemailer.createTransport({ ...mailConfig });
 
 module.exports = transporter;
--- src/email/EmailService.js
+++ src/email/EmailService.js
@@ -1,12 +1,25 @@
 // at this point we need to be sure that email was send and THEN create user.
 const transporter = require('../config/emailTransporter');
+const nodemailer = require('nodemailer');
+
 const sendAccountActivation = async (email, token) => {
-  await transporter.sendMail({
+  const info = await transporter.sendMail({
     from: 'My App <info@my-app.com>',
     to: email,
     subject: 'Account Activation',
-    html: `Token is ${token}`,
+    html: `
+    <div>
+      <b> Please click link below to activate your account </b>
+    </div>
+    <div>
+    <a href="http::/localhost:8080/#/login?token=${token}">ACTIVATE</a>
+    </div>
+    Token is ${token}`,
   });
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log('url: ' + nodemailer.getTestMessageUrl(info));
+  }
 };
 
 module.exports = { sendAccountActivation };
